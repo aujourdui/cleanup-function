79ab7726ebf63cd657442fd4d4d1d255639c145f
kyosuke <kyosuke.ito@gmail.com>
2022-03-15 12:18:08 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+const INITIAL_COUNT = 0;
+
+const Timer = () => {
+  const [count, setCount] = useState(INITIAL_COUNT);
+
+  const countReset = () => {
+    setCount(INITIAL_COUNT);
+  };
+
+  const countIncrement = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  useEffect(() => {
+    console.log("acted!");
+    const timer = setInterval(countIncrement, 1000);
+    return () => {
+      console.log("deleted");
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>current:{count}</p>
+      <button onClick={countReset}>RESET</button>
     </div>
+  );
+};
+
+export default function App() {
+  const [display, toggleDisplay] = useState(false);
+
+  const handleToggleDisplay = () => {
+    toggleDisplay(!display);
+  };
+
+  return (
+    <>
+      <button onClick={handleToggleDisplay}>
+        {display ? "hidden" : "visible"}
+      </button>
+      {display && <Timer />}
+    </>
   );
 }
 
-export default App;
+// function App() {
+//   const [count, setCount] = useState(0);
+//   const [countSecond, setCountSecond] = useState(0);
+//   const [seconds, setSeconds] = useState(0);
+
+//   const addCount = () => {
+//     setCount(count + 1);
+//   };
+
+//   const subtractSecondCount = () => {
+//     setCountSecond(countSecond - 1);
+//   };
+
+// const timer = () => {
+//   set
+// }
+
+// useEffect(() => {
+//   const interval = setInterval(() => {
+//     setSeconds((seconds) => seconds + 1);
+//   }, 1000);
+//   // return () => clearInterval(interval);
+// }, []);
+
+//   return (
+//     <div className="App">
+//       <button onClick={addCount}>Add</button>
+//       <button onClick={subtractSecondCount}>Subtract</button>
+//       {/* <button onClick={seconds}>Seconds</button> */}
+//       <p>{count}</p>
+//       <p>{countSecond}</p>
+//       <p>{seconds}</p>
+//     </div>
+//   );
+// }
+
+// export default App;
